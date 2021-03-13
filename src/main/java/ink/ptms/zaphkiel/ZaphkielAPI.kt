bd8ef9d78359b5f94a011021e4b6250d9e0320b9
sky <sky@asgard.me>
2021-03-14 01:15:11 +0800

    1.4.2 update ItemAPI

--- src/main/java/ink/ptms/zaphkiel/ZaphkielAPI.kt
+++ src/main/java/ink/ptms/zaphkiel/ZaphkielAPI.kt
@@ -160,7 +160,7 @@ object ZaphkielAPI {
                 var group: Group? = null
                 val conf = Files.load(file)
                 if (conf.contains("__group__")) {
-                    val name = file.path.substring(0, file.name.indexOf("."))
+                    val name = file.name.substring(0, file.name.indexOf("."))
                     group = Group(name, file, conf.getConfigurationSection("__group__")!!, priority = conf.getInt("__group__.priority"))
                     registeredGroup[name] = group
                 }
