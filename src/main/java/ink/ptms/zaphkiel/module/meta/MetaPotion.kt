307d30c17498782c487fc59cfa196e17cfc3dd22
sky <sky@asgard.me>
2021-03-14 00:43:42 +0800

    1.4.1 udpate

--- src/main/java/ink/ptms/zaphkiel/module/meta/MetaPotion.kt
+++ src/main/java/ink/ptms/zaphkiel/module/meta/MetaPotion.kt
@@ -2,9 +2,13 @@ package ink.ptms.zaphkiel.module.meta
 
 import ink.ptms.zaphkiel.ZaphkielAPI
 import ink.ptms.zaphkiel.api.Item
+import io.izzel.taboolib.kotlin.warning
+import org.bukkit.Bukkit
 import org.bukkit.inventory.meta.ItemMeta
 import org.bukkit.inventory.meta.PotionMeta
+import org.bukkit.potion.PotionData
 import org.bukkit.potion.PotionEffect
+import org.bukkit.potion.PotionType
 import org.bukkit.util.NumberConversions
 
 @MetaKey("potion")
@@ -15,8 +19,17 @@ class MetaPotion(item: Item) : Meta(item) {
             .map { PotionEffect(ZaphkielAPI.asPotionEffect(it.key)!!, NumberConversions.toInt(it.value.toString().split("-")[0]), NumberConversions.toInt(it.value.toString().split("-").getOrElse(1) { 0 })) }
             .toList()
 
+    val basePotion = item.config.getString("meta.potion.base")
+
     override fun build(itemMeta: ItemMeta) {
         if (itemMeta is PotionMeta) {
+            if (basePotion != null) {
+                try {
+                    itemMeta.basePotionData = PotionData(PotionType.valueOf(basePotion.toUpperCase()))
+                } catch (ignored: Throwable) {
+                    warning("Unknown base potion: $basePotion")
+                }
+            }
             potions.forEach { itemMeta.addCustomEffect(it, true) }
         }
     }
