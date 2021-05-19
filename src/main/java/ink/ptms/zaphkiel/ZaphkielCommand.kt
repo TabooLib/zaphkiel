3d3a5b5af5dcec0535719568197b4fde93d5a772
sky <sky@asgard.me>
2021-05-19 20:56:52 +0800

    update command

--- src/main/java/ink/ptms/zaphkiel/ZaphkielCommand.kt
+++ src/main/java/ink/ptms/zaphkiel/ZaphkielCommand.kt
@@ -18,54 +18,46 @@ import org.bukkit.util.NumberConversions
 @BaseCommand(name = "Zaphkiel", aliases = ["zl", "item"], permission = "*")
 class ZaphkielCommand : BaseMainCommand() {
 
-    @SubCommand(priority = 0.0, description = "赋予物品")
-    val give = object : BaseSubCommand() {
-
-        override fun getArguments() = of(
-            Argument("节点") { ZaphkielAPI.registeredItem.keys.toList() },
-            Argument("玩家", false),
-            Argument("数量", false)
-        )
-
-        override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>) {
-            val item = ZaphkielAPI.registeredItem[args[0]]
-            if (item == null) {
-                notify(sender, "物品 \"&f${args[0]}&7\" 不存在.")
-                return
-            }
-            val target: Player? = when {
-                args.size > 1 -> Bukkit.getPlayerExact(args[1])
-                sender is Player -> sender
-                else -> null
-            }
-            if (target == null) {
-                notify(sender, "缺少玩家.")
-                return
-            }
-            val itemStack = item.build(target).save()
-            if (args.size > 2) {
-                itemStack.amount = NumberConversions.toInt(args[2])
-            }
-            CronusUtils.addItem(target, itemStack)
+    override fun onTabComplete(sender: CommandSender, command: String, argument: String): List<String>? {
+        return when (argument) {
+            "节点" -> ZaphkielAPI.registeredItem.keys.toList()
+            else -> null
         }
     }
 
-    @SubCommand(priority = 0.1, description = "列出物品", type = CommandType.PLAYER)
-    val list = object : BaseSubCommand() {
-
-        override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>) {
-            (sender as Player).openGroupMenu()
+    @SubCommand(priority = 0.0, description = "赋予物品", arguments = ["节点", "玩家?", "数量?"])
+    fun give(sender: CommandSender, args: Array<String>) {
+        val item = ZaphkielAPI.registeredItem[args[0]]
+        if (item == null) {
+            notify(sender, "物品 \"&f${args[0]}&7\" 不存在.")
+            return
         }
+        val target: Player? = when {
+            args.size > 1 -> Bukkit.getPlayerExact(args[1])
+            sender is Player -> sender
+            else -> null
+        }
+        if (target == null) {
+            notify(sender, "缺少玩家.")
+            return
+        }
+        val itemStack = item.build(target).save()
+        if (args.size > 2) {
+            itemStack.amount = NumberConversions.toInt(args[2])
+        }
+        CronusUtils.addItem(target, itemStack)
+    }
+
+    @SubCommand(priority = 0.1, description = "列出物品")
+    fun list(player: Player, args: Array<String>) {
+        player.openGroupMenu()
     }
 
     @SubCommand(priority = 0.2, description = "重载插件")
-    val reload = object : BaseSubCommand() {
-
-        override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>) {
-            Zaphkiel.conf.reload()
-            Zaphkiel.reload()
-            notify(sender, "插件已重载.")
-        }
+    fun reload(sender: CommandSender, args: Array<String>) {
+        Zaphkiel.conf.reload()
+        Zaphkiel.reload()
+        notify(sender, "插件已重载.")
     }
 
     fun notify(sender: CommandSender, value: String) {
