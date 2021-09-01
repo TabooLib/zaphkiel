c2e272139ce010a5e64c0d86d1c8b09b70dc61c4
坏黑 <b@skymc.me>
2021-09-01 21:39:41 +0800

    [1.6.0] update taboolib

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("io.izzel.taboolib") version "1.24"
+    id("io.izzel.taboolib") version "1.26"
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
 }
 
@@ -23,7 +23,7 @@ taboolib {
     install("module-ui")
     install("platform-bukkit")
     classifier = null
-    version = "6.0.0-pre56"
+    version = "6.0.0-52"
 }
 
 repositories {
