d851bddf625e56df67d0b09ac2a3a4da7f9aa64e
坏黑 <b@skymc.me>
2021-12-02 19:41:08 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ taboolib {
     install("platform-bukkit")
     install("expansion-command-helper", "expansion-player-database")
     classifier = null
-    version = "6.0.6-8"
+    version = "6.0.6-10"
 }
 
 repositories {
