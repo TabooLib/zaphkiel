a118373b27f338845ef47966367e2080bbca0528
坏黑 <b@skymc.me>
2021-12-11 22:29:51 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
-    id("io.izzel.taboolib") version "1.31"
+    id("io.izzel.taboolib") version "1.32"
     id("org.jetbrains.kotlin.jvm") version "1.5.10"
     id("org.jetbrains.dokka") version "1.5.30"
 }
@@ -28,7 +28,7 @@ taboolib {
     install("platform-bukkit")
     install("expansion-command-helper", "expansion-player-database")
     classifier = null
-    version = "6.0.6-14"
+    version = "6.0.6-24"
 }
 
 repositories {
