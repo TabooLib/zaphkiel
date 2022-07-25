c2c470af099d172f667a7332caad5fb5de9d1395
坏黑 <b@skymc.me>
2022-07-25 16:53:51 +0800

    2.0.2

---
+++ project/common-impl/src/main/kotlin/ink/ptms/zaphkiel/impl/feature/compat/AttributePlusHook.kt
@@ -0,0 +1,44 @@
+package ink.ptms.zaphkiel.item.compat
+
+import ink.ptms.zaphkiel.ZaphkielAPI
+import org.bukkit.entity.Player
+import org.serverct.ersha.api.AttributeAPI
+import org.serverct.ersha.attribute.data.AttributeData
+import taboolib.common.platform.event.OptionalEvent
+import taboolib.common.platform.event.SubscribeEvent
+import taboolib.common.reflect.Reflex.Companion.getProperty
+import taboolib.common5.Coerce
+import taboolib.platform.util.isAir
+import taboolib.platform.util.isNotAir
+import taboolib.type.BukkitEquipment
+
+internal object AttributePlusHook {
+
+    @SubscribeEvent(bind = "org.serverct.ersha.api.event.AttrUpdateAttributeEvent\$After")
+    fun e1(e: OptionalEvent) {
+        val attributeData = e.source.getProperty<AttributeData>("attributeData")!!
+        val sourceEntity = attributeData.sourceEntity
+        if (sourceEntity is Player) {
+            val attrData = AttributeAPI.getAttrData(sourceEntity)
+            val items = BukkitEquipment.values().mapNotNull { it.getItem(sourceEntity) }
+            items.forEachIndexed { index, item ->
+                AttributeAPI.takeSourceAttribute(attrData, "Zaphkiel.$index")
+                if (item.isAir()) {
+                    return
+                }
+                val itemStream = ZaphkielAPI.read(item)
+                if (itemStream.isVanilla()) {
+                    return
+                }
+                val attribute = itemStream.getZaphkielData()["attribute-plus"]?.asCompound() ?: return
+                val map = HashMap<String, Array<Number>>()
+                attribute.forEach { (key, data) ->
+                    val args = data.asString().split("-")
+                    map[key] = arrayOf(Coerce.toDouble(args[0]), Coerce.toDouble(args.getOrElse(1) { args[0] }))
+                }
+                AttributeAPI.addSourceAttribute(attrData, "Zaphkiel.$index", map, false)
+            }
+        }
+    }
+
+}
---
+++ project/common-impl/src/main/kotlin/ink/ptms/zaphkiel/impl/feature/kether/ActionBuilder.kt
@@ -0,0 +1,92 @@
+package ink.ptms.zaphkiel.item.kether
+
+import ink.ptms.zaphkiel.api.event.ItemBuildEvent
+import ink.ptms.zaphkiel.api.event.ItemReleaseEvent
+import org.bukkit.event.Cancellable
+import org.bukkit.event.Event
+import taboolib.common5.Coerce
+import taboolib.library.kether.ArgTypes
+import taboolib.library.xseries.parseToMaterial
+import taboolib.module.kether.*
+
+/**
+ * Zaphkiel
+ * ink.ptms.zaphkiel.item.kether.ActionBuilder
+ *
+ * @author sky
+ * @since 2021/10/20 10:42 下午
+ */
+object ActionBuilder {
+
+    @KetherParser(["cancel"], namespace = "zaphkiel")
+    fun cancel() = scriptParser {
+        actionNow {
+            val e = itemEvent<Event>()
+            if (e is Cancellable) {
+                e.isCancelled = true
+            }
+        }
+    }
+
+    /**
+     * build name *123 to *123
+     * build lore *123 to *123
+     * build icon *stone
+     * build data *1
+     */
+    @KetherParser(["build"], namespace = "zaphkiel-build")
+    fun build() = scriptParser {
+        it.switch {
+            case("icon") {
+                val value = it.next(ArgTypes.ACTION)
+                actionNow {
+                    newFrame(value).run<Any>().thenAccept { value ->
+                        itemEvent<ItemReleaseEvent>().icon = value.toString().parseToMaterial()
+                    }
+                }
+            }
+            case("data") {
+                val value = it.next(ArgTypes.ACTION)
+                actionNow {
+                    newFrame(value).run<Any>().thenAccept { value ->
+                        itemEvent<ItemReleaseEvent>().data = Coerce.toInteger(value.toString())
+                    }
+                }
+            }
+            case("name") {
+                val key = it.next(ArgTypes.ACTION)
+                it.expects("to")
+                val value = it.next(ArgTypes.ACTION)
+                actionNow {
+                    val itemEvent = itemEvent<Event>()
+                    newFrame(key).run<Any>().thenApply { key ->
+                        newFrame(value).run<Any>().thenApply { value ->
+                            when (itemEvent) {
+                                is ItemBuildEvent.Pre -> itemEvent.addName(key.toString(), value)
+                                is ItemReleaseEvent.Display -> itemEvent.addName(key.toString(), value)
+                                else -> error("not a build or release event")
+                            }
+                        }
+                    }
+                }
+            }
+            case("lore") {
+                val key = it.next(ArgTypes.ACTION)
+                it.expects("to")
+                val value = it.next(ArgTypes.ACTION)
+                actionNow {
+                    val itemEvent = itemEvent<Event>()
+                    newFrame(key).run<Any>().thenApply { key ->
+                        newFrame(value).run<Any>().thenApply { value ->
+                            when (itemEvent) {
+                                is ItemBuildEvent.Pre -> itemEvent.addLore(key.toString(), value)
+                                is ItemReleaseEvent.Display -> itemEvent.addLore(key.toString(), value)
+                                else -> error("not a build or release event")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
